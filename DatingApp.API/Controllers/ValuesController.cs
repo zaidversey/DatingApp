2a44174562d39e675289ec616bea537e0ff4e21d
Zaid Versey <zaidversey@gmail.com>
2019-08-05 18:57:24 -0400

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -25,6 +25,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
