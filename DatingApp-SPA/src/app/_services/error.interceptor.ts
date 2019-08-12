0bd5d7723e307d90e6ca5e400d95460ae03ab430
Zaid Versey <zaidversey@gmail.com>
2019-08-11 23:54:03 -0400

    End of section 5

---
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError( error => {
+                if (error instanceof HttpErrorResponse) {
+                    if (error.status === 401) {
+                        return throwError(error.statusText);
+                    }
+                    const applicationError = error.headers.get('Application-Error');
+                    if (applicationError) {
+                        console.error(applicationError);
+                        return throwError(applicationError);
+                    }
+                    const serverError = error.error.errors;
+                    let modalStateErrors = '';
+                    if (serverError && typeof serverError === 'object') {
+                        for (const key in serverError) {
+                            if (serverError[key]) {
+                                modalStateErrors += serverError[key] + '\n';
+                            }
+                        }
+                    }
+                    return throwError(modalStateErrors || serverError || 'Server Error');
+                }
+            })
+        );
+    }
+ }
+
+ export const ErrorInterceptorProvider = {
+     // tslint:disable-next-line: comment-format
+     provide: HTTP_INTERCEPTORS, // from array of Interceptors
+     useClass: ErrorInterceptor, // interceptor method - the method above
+     multi: true // doesn't replace interceptors, add the interceptor ot=f already existing ones
+ };
